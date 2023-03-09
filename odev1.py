00aa46535dd61be898b23d6983ee53c9b6d356f7
uur28 <127440128+uur28@users.noreply.github.com>
2023-03-09 23:06:30 +0300

    Add files via upload

---
+++ odev1.py
@@ -0,0 +1,22 @@
+#str   x="pyhton kampı"
+#int   x=16
+#float  x=52.5
+#bool  x=false
+#list   x={"ayse""merve""fatma"}
+
+
+#list : eğitmenlerin seçildiği kısım list veri tipindedir
+#str : isim soyisim yazdığımız kısımlar str veri tipindedir
+#bool:siteye girerken yazdığımız şifrenin true false olması durumu bu kısmın booll veri tipi olduğunu gösterir
+#int: adress kısmında posta  sorulan bölümde yazacağımız değer int veri tipinde olacaktır
+
+# e posta onayıyla hesap aktiflesiitrme 
+
+ePostaOnay = False
+
+if ePostaOnay == False :
+    print("hesabınız aktif degil")
+    print("aktiflesirmek ister misiniz?:")
+    input()
+else :
+    print("hesabınız aktif")
